4306ed275881849a6e2d2dfc7ee7d3834415c3ad
Himal Gautam <himalpgautam@gmail.com>
2022-05-26 07:19:23 +0530

    .

--- src/routers/router.js
+++ src/routers/router.js
@@ -10,9 +10,9 @@ const router = new express.Router();
 router.post("/users/password-reset/get-email", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
-    if(!user){
+    if (!user) {
       res.status(400).send({ success: false });
-    }else{
+    } else {
       const resAuth = await Auth(
         req.body.email,
         "and resetting password. Please click the link https://password-reset-guvi.netlify.app/password-reset"
@@ -26,26 +26,41 @@ router.post("/users/password-reset/get-email", async (req, res) => {
   }
 });
 
-
 //update password
 router.patch("/users/password-reset/reset", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
-    if(!user){
-      res.status(400).send({ success: false, msg: "User not found with email" });
-    }else{
-      if(user.otp === req.body.otp){
-        user["password"] = req.body.password;  
-        await user.save();
-        res.status(200).send({ success: true, msg: "Task Completed"});
-      }
-      else{
-        res.status(400).send({ success: false, msg: "OTP Mismatch"}); 
-      }
+    if (!user) {
+      res
+        .status(400)
+        .send({ success: false, msg: "User not found with email" });
+    } else {
+      user["password"] = req.body.password;
+      await user.save();
+      res.status(200).send({ success: true, msg: "Task Completed" });
     }
   } catch (e) {
     res.status(400).send(e);
   }
 });
 
+//confirm otp
+router.post("/users/password-reset/otp", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      res
+        .status(400)
+        .send({ success: false, msg: "User not found with email" });
+    } else {
+      if (user.otp === req.body.otp) {
+        res.status(200).send({ success: true, msg: "OTP matched" });
+      } else {
+        res.status(400).send({success: false, msg: "OTP didn't matched"});
+      }
+    }
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 export default router;
